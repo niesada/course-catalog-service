9bc051212c2ec91075820b7823b91e5a0d6a26b9
Eddie.Nariyoshi <Eddie.Nariyoshi@dendreon.com>
2023-11-19 12:28:51 -0800

    integration and unit test

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,13 +27,16 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	//logging //use KLogging()
+	//logging //for KLogging()
 	implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
 
 	runtimeOnly("com.h2database:h2")
 //	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+//for
+	testImplementation("io.mockk:mockk:1.10.4")
+	testImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 tasks.withType<KotlinCompile> {
