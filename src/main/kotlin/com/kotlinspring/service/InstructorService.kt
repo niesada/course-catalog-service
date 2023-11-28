3337d8d9ab94b7ae3b6db4c89d6dfdca9720acd6
Eddie.Nariyoshi <Eddie.Nariyoshi@dendreon.com>
2023-11-27 21:35:08 -0800

    wip

---
+++ src/main/kotlin/com/kotlinspring/service/InstructorService.kt
@@ -0,0 +1,12 @@
+package com.kotlinspring.service
+
+import com.kotlinspring.dto.InstructorDTO
+import org.springframework.stereotype.Service
+
+@Service
+class InstructorService {
+    fun createInstructor(instructorDTO: InstructorDTO): Any {
+
+    }
+
+}
