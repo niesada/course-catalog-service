70b6d7186e8fc2fe479ad05dfa0cc39d0eeabcd1
Eddie.Nariyoshi <Eddie.Nariyoshi@dendreon.com>
2023-12-03 12:01:27 -0800

    Integrate with Postgres DB

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,8 +31,9 @@ dependencies {
 	//logging //for KLogging()
 	implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
 
-	runtimeOnly("com.h2database:h2")
-//	runtimeOnly("org.postgresql:postgresql")
+//	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 //for
